d743a2b0f7e79a654e39cc15b1d9c8431260b35a
Adam Watkins <adam@stupidpupil.co.uk>
2021-12-11 21:04:41 +0000

    Add ModWrapper class to generate mod bundles

---
+++ lib/wrsr_skinner/mod_wrapper.rb
@@ -0,0 +1,74 @@
+module WRSRSkinner
+
+  class ModWrapper
+
+    def self.skinnable_ids_with_immediate_dependents(skinnable_ids)
+      skinnable_ids.map {|i| Skinnable.new(i).depends_on}.inject(skinnable_ids, :+).uniq.sort
+    end
+
+    def self.skinnable_ids_with_all_dependents(skinnable_ids)
+      ret = ModWrapper.skinnable_ids_with_immediate_dependents(skinnable_ids)
+
+      if ModWrapper.skinnable_ids_with_immediate_dependents(ret) != ret
+        return ModWrapper.skinnable_ids_with_all_dependents(ret)
+      end
+
+      return ret
+    end
+
+    # These constraints are meant to prevent returning any bundle
+    # whose generation seems to have gone badly wrong
+    BundleMaxFileCount = 200
+    BundleMaxSizeBytes = 100*1024*1024
+
+    def initialize(requested_skinnable_ids, brand, mod_id = nil, steam_owner_id = nil)
+
+      if not requested_skinnable_ids.is_a? Array then
+        raise "requested_skinnable_ids is not an Array!"
+      end
+
+      if requested_skinnable_ids.count > 16 then
+        raise "requested_skinnable_ids has more than 16 members!"
+      end
+
+      @requested_skinnable_ids = requested_skinnable_ids
+      @included_skinnable_ids = ModWrapper.skinnable_ids_with_all_dependents(requested_skinnable_ids)
+      @brand = brand
+      @mod_id = mod_id
+      @steam_owner_id = steam_owner_id
+    end
+
+    def zip_io
+
+      zip_io = StringIO.new
+
+      Dir.mktmpdir() do |temp_dir|
+        skinnables = @included_skinnable_ids.map { |sk| Skinnable.new(sk, temp_dir) }
+
+        if Parallel then
+          Parallel.each(skinnables,  in_processes: 6) {|s| s.save_textures_with_brand(@brand)}
+        else
+          skinnables.each {|s| s.save_textures_with_brand(@brand)}
+        end
+
+        files_to_be_zipped = Dir[ File.join( temp_dir, "**", "**" ) ]
+
+        break if files_to_be_zipped.count > BundleMaxFileCount
+        files_to_be_zipped_size = files_to_be_zipped.map {|f| File.file?(f) ? File.size(f) : 0 }.inject(:+)
+        break if files_to_be_zipped_size > BundleMaxSizeBytes
+
+        Zip::File.open_buffer(zip_io) do |zip_file|
+          files_to_be_zipped.each do |file|
+            zip_file.add( file.sub( "#{ temp_dir }/", "" ), file )
+          end
+        end
+        
+      end
+
+      zip_io.rewind
+      return zip_io
+    end
+
+  end
+
+end
